a48d685d1d177db424a7af0d48712b5d8fb6aa55
Hernán David Moreno <hernandavidmoreno@Hernans-MacBook-Pro.local>
2022-05-27 00:54:14 -0300

    solved case-2

--- list-search/case-2/src/App.tsx
+++ list-search/case-2/src/App.tsx
@@ -1,14 +1,16 @@
-import type {Product} from "./types";
+import React, { useEffect, useState } from "react";
 
-import {useEffect, useState} from "react";
-
+import type { Product } from "./types";
 import api from "./api";
+import { useDebounce } from "./hooks/useDebounce";
 
-function Recommended() {
+const MemoizedRecommended = React.memo(function Recommended() {
   const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
-    api.search().then(setProducts);
+    api.search().then((data) => {
+      setProducts(data.map((item) => ({ ...item, fav: false })));
+    });
   }, []);
 
   return (
@@ -28,23 +30,63 @@ function Recommended() {
       </ul>
     </main>
   );
-}
+});
 
 function App() {
   const [products, setProducts] = useState<Product[]>([]);
+  const [favouriteProducts, setFavouriteProducts] = useState<Product["id"][]>(
+    () => JSON.parse(localStorage.getItem("favouriteProducts") || "[]")
+  );
   const [query, setQuery] = useState<string>("");
 
+  const debouncedValue = useDebounce(query, 800);
+
   useEffect(() => {
-    api.search(query).then(setProducts);
-  }, [query]);
+    api.search(debouncedValue).then((data) => {
+      setProducts(
+        data.map((item) =>
+          favouriteProducts?.includes(item.id) ? { ...item, fav: true } : item
+        )
+      );
+    });
+  }, [debouncedValue]);
+
+  useEffect(() => {
+    console.log(typeof favouriteProducts);
+    localStorage.setItem(
+      "favouriteProducts",
+      JSON.stringify(favouriteProducts)
+    );
+  }, [favouriteProducts]);
+
+  const handleSetFavouriteItem = (id: Product["id"]) => {
+    const updatedProducts = products.map((item) =>
+      item.id === id ? { ...item, fav: !item.fav } : item
+    );
+    setProducts(updatedProducts);
+    setFavouriteProducts(
+      updatedProducts
+        .filter((product) => product.fav)
+        ?.map((product) => product.id)
+    );
+  };
 
   return (
     <main>
       <h1>Tienda digitaloncy</h1>
-      <input name="text" placeholder="tv" type="text" onChange={(e) => setQuery(e.target.value)} />
+      <input
+        name="text"
+        placeholder="tv"
+        type="text"
+        onChange={(e) => setQuery(e.target.value)}
+      />
       <ul>
         {products.map((product) => (
-          <li key={product.id}>
+          <li
+            key={product.id}
+            onClick={() => handleSetFavouriteItem(product.id)}
+            className={product.fav ? "fav" : ""}
+          >
             <h4>{product.title}</h4>
             <p>{product.description}</p>
             <span>$ {product.price}</span>
@@ -52,7 +94,7 @@ function App() {
         ))}
       </ul>
       <hr />
-      <Recommended />
+      <MemoizedRecommended />
     </main>
   );
 }
---
+++ list-search/case-2/src/hooks/useDebounce.ts
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+
+export function useDebounce(value: any, delay: number) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handle = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => clearTimeout(handle);
+  }, [value, delay]);
+
+  return debouncedValue;
+}
